9c9b85e3af15093031e0ff6e969bad064cdb5fd8
Erik Alexandersson <work@erikalexandersson.se>
2017-12-02 13:34:28 +0100

    Day 2 completed!

---
+++ src/test/java/adventofcode/Day2Part1Test.java
@@ -0,0 +1,39 @@
+package adventofcode;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Day2Part1Test {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		int arr[][] = { { 5, 1, 9, 5 }, { 7, 5, 3 }, { 2, 4, 6, 8 } };
+		int expected = 18;
+
+		int checksum = Day2Part1.getChecksum(arr);
+
+		assertEquals(expected, checksum);
+	}
+
+}
---
+++ src/test/java/adventofcode/Day2Part2Test.java
@@ -0,0 +1,39 @@
+package adventofcode;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Day2Part2Test {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		int arr[][] = { { 5, 9, 2, 8 }, { 9, 4, 7, 3 }, { 3, 8, 6, 5 } };
+		int expected = 9;
+
+		int checksum = Day2Part2.getChecksum(arr);
+
+		assertEquals(expected, checksum);
+	}
+
+}
