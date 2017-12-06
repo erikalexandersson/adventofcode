30fa5590da11045fd75b45c5ac78bc83666e5fcf
Erik Alexandersson <work@erikalexandersson.se>
2017-12-06 18:19:25 +0100

    Day 6 completed!

---
+++ src/main/java/adventofcode/Day6Part1.java
@@ -0,0 +1,69 @@
+package adventofcode;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Day6Part1 {
+
+	public static int[] reallocate(int[] arr) {
+
+		int max = Integer.MIN_VALUE;
+		int pos = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			int oldMax = max;
+			max = Math.max(max, arr[i]);
+			if (max > oldMax) {
+				pos = i;
+			}
+		}
+
+		int toRedistribute = arr[pos];
+		arr[pos] = 0;
+
+		while (toRedistribute-- > 0) {
+			if (++pos >= arr.length) {
+				pos = 0;
+			}
+			arr[pos]++;
+		}
+
+		return arr;
+	}
+
+	public static int redistribute(int[] arr) {
+		int cycle = 0;
+
+		Set<String> keys = new HashSet<>();
+
+		boolean unique = true;
+		do {
+			arr = reallocate(arr);
+			cycle++;
+			unique = keys.add(Arrays.toString(arr));
+		} while (unique);
+
+		return cycle;
+	}
+
+	public static void main(String args[]) {
+		// int arr[] = { 5, 1, 10, 0, 1, 7, 13, 14, 3, 12, 8, 10, 7, 12, 0, 6 };
+
+		// System.out.println(redistribute(arr));
+
+		speedTest();
+	}
+
+	public static void speedTest() {
+		int arr[] = { 14, 0, 15, 12, 11, 11, 3, 5, 1, 6, 8, 4, 9, 1, 8, 4 };
+
+		final long startTime = System.nanoTime();
+		int result = redistribute(arr);
+		final long endTime = System.nanoTime() - startTime;
+
+		System.out.println(result + " cycles");
+		System.out.println(endTime / 1000.0 / 1000.0 + " ms");
+	}
+
+}
---
+++ src/test/java/adventofcode/Day6Part2Test.java
@@ -0,0 +1,39 @@
+package adventofcode;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Day6Part2Test {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		int arr[] = { 0, 2, 7, 0 };
+		int expected = 4;
+
+		int actual = Day6Part2.countCycles(arr);
+
+		assertEquals(expected, actual);
+	}
+
+}
